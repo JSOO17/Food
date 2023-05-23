6d4b4406320f5516da800b7e0afdd6b39e2b2800
JaiderOsorio <JaiderOsorio@DESKTOP-DC3SJGO>
2023-05-23 10:03:54 -0500

    Restaurant Services

---
+++ Foods.Application/Services/Interfaces/IRestaurantServices.cs
@@ -0,0 +1,13 @@
+﻿using Foods.Application.DTO.Request;
+using Foods.Application.DTO.Response;
+using Foods.Domain.Models;
+
+namespace Foods.Application.Services.Interfaces
+{
+    public interface IRestaurantServices
+    {
+        Task<List<ItemRestaurantResponseDTO>> GetRestaurants(int page, int count);
+        Task<RestaurantResponseDTO> CreateRestaurant(RestaurantRequestDTO restaurant);
+        Task<RestaurantEmployeesResponseDTO> CreateEmployeeRestaurant(RestaurantEmployeesRequestDTO restaurant, long userId);
+    }
+}
